c6c04ca0501ad5a042b0a7e21a8c1babf32e0967
andrescano5 <andrescano7.ac@gmail.com>
2021-12-25 17:18:37 -0300

    Feat: setup DB

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 //GET nosotros page
-router.get('/nosotros', (req, res) => {
-  res.render('pages/nosotros', { title: 'Nosotros' });
+router.get("/nosotros", (req, res) => {
+  res.render("pages/nosotros", { title: "Nosotros" });
 });
 
 //GET nosotros page
-router.get('/contacto', (req, res) => {
-  res.render('pages/contacto', { title: 'Contacto' });
+router.get("/contacto", (req, res) => {
+  res.render("pages/contacto", { title: "Contacto" });
 });
 
 module.exports = router;
